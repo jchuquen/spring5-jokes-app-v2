2983adfe17e6b839da753e8fba70b864d497e0d5
Jorge Chuquen <jorge.chuquen+DFIN@dfinsolutions.com>
2024-02-15 19:52:10 -0500

    Spring project created. Closes #1

---
+++ src/main/java/jchuquen/study/java/spring5/jokes/app/Spring5JokesAppApplication.java
@@ -0,0 +1,13 @@
+package jchuquen.study.java.spring5.jokes.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5JokesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5JokesAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jchuquen/study/java/spring5/jokes/app/Spring5JokesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package jchuquen.study.java.spring5.jokes.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5JokesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
